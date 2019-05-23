c13ca2db4a53133514c0282b3fefe03c46f881c4
elbywan <elbywan@hotmail.com>
2019-05-23 13:47:38 +0200

    tests tools: better nested i18n support for key/options extraction

--- src/tests/tools.ts
+++ src/tests/tools.ts
@@ -1,7 +1,13 @@
 export function getMessageKey(message) {
+    if(typeof message === 'string') {
+        return JSON.parse(message).key
+    }
     return JSON.parse(message.text).key
 }
 
 export function getMessageOptions(message) {
+    if(typeof message === 'string') {
+        return JSON.parse(message).options
+    }
     return JSON.parse(message.text).options
 }
