db5c4af72fbcccea4ba66d6b2c14faaa53130a70
elbywan <elbywan@hotmail.com>
2019-06-21 12:06:44 +0200

    increase jest max workers

--- src/actions/test.ts
+++ src/actions/test.ts
@@ -3,11 +3,11 @@ import jest from 'jest'
 
 const configLocation = path.join(__dirname, '../../jest.config.js')
 
-export function test (files, { target, sandbox }) {
+export function test (files: string | undefined, { target, sandbox }) {
     global['runnerTarget'] = target
     global['sandboxedRunner'] = !!sandbox
 
     jest.run([
-        `--config=${configLocation}`
+        `--config=${configLocation}`, '--maxWorkers=20'
     ], files)
 }
